33f55cd8e27bd32c5cca794cb0532e71c3570260
Peter James Flanagan <pjflanagan1@gmail.com>
2020-04-29 00:01:32 -0400

    format

--- Cities/main.js
+++ Cities/main.js
@@ -3,16 +3,16 @@
 
 window.onload = function () {
 
-  // the canvas is an element in our html that we reference here by id
+	// the canvas is an element in our html that we reference here by id
 	const canvas = document.getElementById("pix");
 	const ctx = canvas.getContext("2d");
 
-  // we want this to take up the whole page, so we get the window height and width here
+	// we want this to take up the whole page, so we get the window height and width here
 	const W = window.innerWidth, H = window.innerHeight;
 	canvas.width = W;
 	canvas.height = H;
 
-  // world is defined by us, first we initialize it and then we run the program
+	// world is defined by us, first we initialize it and then we run the program
 	const world = new World(ctx, W, H);
 	world.run();
 }
--- Cities/objects.js
+++ Cities/objects.js
@@ -3,8 +3,8 @@
 // this is a good place to store constants
 
 const WORLD = {
-  BACKGROUND: '#29073b',
-  BUILDING_COUNT: 100
+	BACKGROUND: '#29073b',
+	BUILDING_COUNT: 100
 }
 
 const BUILDING = {
@@ -14,68 +14,68 @@ const BUILDING = {
 
 class World {
 	constructor(ctx, width, height) {
-    // the context (ctx) is the reference to the element on page and is where we will draw EVERYTHING
+		// the context (ctx) is the reference to the element on page and is where we will draw EVERYTHING
 		this.ctx = ctx;
 		this.W = width;
 		this.H = height;
 
-    // we will be drawing buildings, so lets store them all in an array
+		// we will be drawing buildings, so lets store them all in an array
 		this.buildings = [];
 
-    // draw the background so the page has color
+		// draw the background so the page has color
 		this.drawBackground();
 
-    // initialize the buildings (make them all exist)
+		// initialize the buildings (make them all exist)
 		this.initBuildings();
 	}
 
 	// INIT
 
 	initBuildings() {
-    // initialize one building for every building in the count
-    for(let b = 0; b < WORLD.BUILDING_COUNT; ++b) {
-      this.buildings.push(new Building(this));
-    }
+		// initialize one building for every building in the count
+		for (let b = 0; b < WORLD.BUILDING_COUNT; ++b) {
+			this.buildings.push(new Building(this));
+		}
 	}
 
 	// ANIMATE
 
 	run() {
-    // this function means the next time the computer is ready to draw
-    // then run the function "animate"
+		// this function means the next time the computer is ready to draw
+		// then run the function "animate"
 		window.requestAnimationFrame(this.animate.bind(this));
 	}
 
 	animate() {
-    // first we will draw the background
+		// first we will draw the background
 		this.drawBackground();
 
-    // then we will draw the buildings on top of the background
+		// then we will draw the buildings on top of the background
 		const done = this.drawBuildings();
 
-    // if we are done drawing buildings then we should stop
-    // otherwise we will animate again the next time the computer is ready
-    if(!done) {
-		  window.requestAnimationFrame(this.animate.bind(this));
-    }
+		// if we are done drawing buildings then we should stop
+		// otherwise we will animate again the next time the computer is ready
+		if (!done) {
+			window.requestAnimationFrame(this.animate.bind(this));
+		}
 	}
 
 	// DRAW
 
 	drawBackground() {
-    // draw a rectangle that covers the whole page
+		// draw a rectangle that covers the whole page
 		this.ctx.rect(0, 0, this.W, this.H);
 		this.ctx.fillStyle = WORLD.BACKGROUND;
-    this.ctx.fill();
+		this.ctx.fill();
 	}
 
 	drawBuildings() {
-    // for each of the buildings, grow in size, then draw them
+		// for each of the buildings, grow in size, then draw them
 		this.buildings.forEach(building => {
-      building.grow();
-      building.draw();
-    });
-    return false;
+			building.grow();
+			building.draw();
+		});
+		return false;
 	}
 }
 
@@ -83,28 +83,28 @@ class World {
 
 class Building {
 	constructor(world) {
-    // hold onto the world object so we can reference height and width
+		// hold onto the world object so we can reference height and width
 		this.world = world;
 
-    // hold onto the context so we can all draw on the same canvas
+		// hold onto the context so we can all draw on the same canvas
 		this.ctx = this.world.ctx;
 
-    this.currentHeight = 0;
+		this.currentHeight = 0;
 
-    // this.eventualHeight = ?;
-    // this.growthRate = ?;
-    // this.width = ?;
-    // this.color = ?;
-    // this.windows = [];
-  
+		// this.eventualHeight = ?;
+		// this.growthRate = ?;
+		// this.width = ?;
+		// this.color = ?;
+		// this.windows = [];
+
 	}
 
 	grow() {
-    // the grow function will have the building increase in size
+		// the grow function will have the building increase in size
 	}
 
 	draw() {
-    // the draw function will have the actual building being drawn
-  }
+		// the draw function will have the actual building being drawn
+	}
 
 }
